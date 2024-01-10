a5db160d56b08638a4a7e0d5614cbacf49e4ef56
Gilles Delhaes <gilles.delhaes@gmail.com>
2024-01-10 15:02:14 +0100

    Flesh out the account system

--- AlertHarvest/alerts_visualization/urls.py
+++ AlertHarvest/alerts_visualization/urls.py
@@ -8,5 +8,12 @@ urlpatterns = [
     path('display/<int:pk>/', views.alert_details, name = "alert_details"),
     path("login/", auth_views.LoginView.as_view(template_name="accounts/login.html"),name='login'),
     path("logout/", auth_views.LogoutView.as_view(next_page='/viz/login/'), name="logout"),
+    path("password_change/", auth_views.PasswordChangeView.as_view(
+        template_name="accounts/password_change.html",
+        success_url="/viz/password_change/done/"
+    ), name="password_change"),
+    path("password_change/done/", auth_views.PasswordChangeDoneView.as_view(
+        template_name="accounts/password_change_done.html"
+    ), name="password_change_done"),
     path('autosave_notes/', views.auto_save_notes, name='auto_save_notes'),
 ]
