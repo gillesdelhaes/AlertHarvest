675d21d4e17d1445abe39fadacba19f36443f310
Gilles Delhaes <gilles.delhaes@gmail.com>
2024-07-18 09:56:22 +0200

    New and improved multi-target notification system with functional email module

--- AlertHarvest/alerts_notificator/notification_modules/email.py
+++ AlertHarvest/alerts_notificator/notification_modules/email.py
@@ -2,6 +2,7 @@ import configparser
 import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
+from pathlib import Path  # Import Path for handling file paths
 
 def read_email_config(file_path='notification_config/email.conf'):
     config = configparser.ConfigParser()
@@ -15,32 +16,45 @@ def read_email_config(file_path='notification_config/email.conf'):
     except KeyError:
         print(f"Error reading 'EmailConfig' section from {file_path}")
         return None
-    email_config = config['EmailConfig']
-    
-    smtp_server = email_config.get('smtp_server')
-    smtp_port = email_config.getint('smtp_port')
-    smtp_username = email_config.get('smtp_username')
-    smtp_password = email_config.get('smtp_password')
-    to_email = email_config.get('to_email')
-    
-    return smtp_server, smtp_port, smtp_username, smtp_password, to_email
 
-def send_notification(alert):
-    smtp_server, smtp_port, smtp_username, smtp_password, to_email = read_email_config()
+    return {
+        'smtp_server': email_config.get('smtp_server'),
+        'smtp_port': email_config.getint('smtp_port'),
+        'smtp_username': email_config.get('smtp_username'),
+        'smtp_password': email_config.get('smtp_password')
+    }
+
+def send_notification(alert, **kwargs):
+    email_config = read_email_config()
+
+    # with open('notification_log.txt', 'a') as file:
+    #     file.write(str(kwargs) + '\n')
+
+    # Read HTML template
+    template_path = Path('notification_config/notification_template.html')
+    with template_path.open('r') as template_file:
+        html_template = template_file.read()
 
     # Create the email message
     subject = 'Alert Notification'
-    body = f"Received alert notification: {alert.source}, {alert.location}, {alert.message}, {alert.last_occurrence}"
-    sender_email = smtp_username
+    body = html_template.format_map({
+        'source': alert.source,
+        'location': alert.location,
+        'severity': alert.severity,
+        'message': alert.message,
+        'last_occurrence': alert.last_occurrence
+    })
+    sender_email = email_config['smtp_username']
 
     message = MIMEMultipart()
     message['From'] = sender_email
-    message['To'] = to_email
+    message['To'] = kwargs.get('to_email')
     message['Subject'] = subject
-    message.attach(MIMEText(body, 'plain'))
+    message.attach(MIMEText(body, 'html'))  # Set the content type to HTML
 
     # Connect to the SMTP server and send the email
-    with smtplib.SMTP(smtp_server, smtp_port) as server:
+    with smtplib.SMTP(email_config['smtp_server'], email_config['smtp_port']) as server:
         server.starttls()
-        server.login(smtp_username, smtp_password)
-        server.sendmail(sender_email, to_email, message.as_string())
+        server.login(email_config['smtp_username'], email_config['smtp_password'])
+        server.sendmail(sender_email, kwargs.get('to_email'), message.as_string())
+
--- AlertHarvest/alerts_notificator/tasks.py
+++ AlertHarvest/alerts_notificator/tasks.py
@@ -1,3 +1,85 @@
+# from celery import shared_task
+# from alerts_api.models import Alert
+# import json
+# import importlib
+
+# @shared_task
+# def process_alert_notification(alert_id):
+#     """
+#     Worker task
+#     """
+#     alert_instance = Alert.objects.get(pk=alert_id)
+#     matching_rule = find_matching_rule(alert_instance)
+#     if matching_rule:
+#         send_notifications(alert_instance, matching_rule)
+
+# def find_matching_rule(alert):
+#     """
+#     Find the first matching rule for the given alert.
+#     """
+#     rules = load_rules_from_file('notification_config/rules.json')  # Implement a function to read rules from the file
+#     for rule in rules:
+#         if evaluate_rule(alert, rule):
+#             return rule
+
+#     return None
+
+# def load_rules_from_file(file_path):
+#     """
+#     Read rules in json
+#     """
+#     with open(file_path, 'r') as file:
+#         rules = json.load(file)
+
+#     # Convert the JSON structure to a dictionary list
+#     converted_rules = []
+#     for rule in rules:
+#         converted_rule = {
+#             'source': rule.get('source', 'Any'),
+#             'location': rule.get('location', 'Any'),
+#             'severity': rule.get('severity', 'Any'),
+#             'targets': rule.get('targets', [])
+#         }
+#         converted_rules.append(converted_rule)
+
+#     return converted_rules
+
+# def evaluate_rule(alert, rule):
+#     """
+#     Evaluate if the alert matches the given rule.
+#     """
+#     source_match = rule['source'] == 'Any' or alert.source == rule['source']
+#     location_match = rule['location'] == 'Any' or alert.location == rule['location']
+#     severity_match = rule['severity'] == 'Any' or alert.severity == rule['severity']
+
+#     return source_match and location_match and severity_match
+
+# def send_notifications(alert, rule):
+#     """
+#     Send the notifications to the right targets
+#     """
+#     for target in rule['targets']:
+#         try:
+#             target_name = target.get('name')
+#             if target_name:
+#                 # Dynamically import the notification module
+#                 module_path = f'alerts_notificator.notification_modules.{target_name}'
+#                 notification_module = importlib.import_module(module_path)
+
+#                 # Get additional parameters from the target dictionary
+#                 additional_params = target.get('additional_params', {})
+
+#                 # Call the send_notification function from the module with additional parameters
+#                 getattr(notification_module, 'send_notification')(alert, **additional_params)
+#             else:
+#                 print("Target name not specified in the rule.")
+            
+#         except ImportError:
+#             print(f"Unsupported target: {target}")
+#         except AttributeError:
+#             print(f"Method 'send_notification' not found in module for target: {target}")
+
+
 from celery import shared_task
 from alerts_api.models import Alert
 import json
@@ -9,64 +91,44 @@ def process_alert_notification(alert_id):
     Worker task
     """
     alert_instance = Alert.objects.get(pk=alert_id)
-    matching_rule = find_matching_rule(alert_instance)
-    if matching_rule:
-        send_notifications(alert_instance, matching_rule)
-
-def find_matching_rule(alert):
-    """
-    Find the first matching rule for the given alert.
-    """
-    rules = load_rules_from_file('notification_config/rules.json')  # Implement a function to read rules from the file
-    for rule in rules:
-        if evaluate_rule(alert, rule):
-            return rule
-
-    return None
-
-def load_rules_from_file(file_path):
-    """
-    Read rules in json
-    """
-    with open(file_path, 'r') as file:
+    with open('notification_config/rules.json', 'r') as file:
         rules = json.load(file)
-
-    # Convert the JSON structure to a dictionary list
-    converted_rules = []
+ 
     for rule in rules:
-        converted_rule = {
-            'source': rule.get('source', 'Any'),
-            'location': rule.get('location', 'Any'),
-            'severity': rule.get('severity', 'Any'),
-            'targets': rule.get('targets', [])
-        }
-        converted_rules.append(converted_rule)
+        if evaluate_and_send_notification(alert_instance, rule):
+            break  # Break after the first matching rule
 
-    return converted_rules
-
-def evaluate_rule(alert, rule):
+def evaluate_and_send_notification(alert, rule):
     """
-    Evaluate if the alert matches the given rule.
+    Evaluate if the alert matches the given rule and send the notification.
+    Return True if a notification is sent, otherwise False.
     """
     source_match = rule['source'] == 'Any' or alert.source == rule['source']
     location_match = rule['location'] == 'Any' or alert.location == rule['location']
     severity_match = rule['severity'] == 'Any' or alert.severity == rule['severity']
 
-    return source_match and location_match and severity_match
+    if source_match and location_match and severity_match:
+        for target in rule.get('targets', []):
+            try:
+                target_name = target.get('name')
+                if target_name:
+                    # Dynamically import the notification module
+                    module_path = f'alerts_notificator.notification_modules.{target_name}'
+                    notification_module = importlib.import_module(module_path)
 
+                    # Get additional parameters from the target dictionary
+                    additional_params = target.get('additional_params', {})
 
-def send_notifications(alert, rule):
-    """
-    Send the notifications to the right targets
-    """
-    for target in rule['targets']:
-        try:
-            # Dynamically import the notification module
-            module_path = f'alerts_notificator.notification_modules.{target}'
-            notification_module = importlib.import_module(module_path)
+                    # Call the send_notification function from the module with additional parameters
+                    getattr(notification_module, 'send_notification')(alert, **additional_params)
+                else:
+                    print("Target name not specified in the rule.")
 
-            # Call the send_notification function from the module
-            getattr(notification_module, 'send_notification')(alert)
-            
-        except ImportError:
-            print(f"Unsupported target: {target}")
+            except ImportError:
+                print(f"Unsupported target: {target}")
+            except AttributeError:
+                print(f"Method 'send_notification' not found in module for target: {target}")
+
+        return True
+    
+    return False
